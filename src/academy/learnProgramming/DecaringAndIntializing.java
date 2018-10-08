1ac083444577d27a1e67628849a733c2aa77da80
venu2804 <venu2804@gmail.com>
2018-10-08 19:47:54 +0100

    First Commit

---
+++ src/academy/learnProgramming/DecaringAndIntializing.java
@@ -0,0 +1,27 @@
+package academy.learnProgramming;
+
+public class DecaringAndIntializing {
+
+    public static void main(String[] args){
+
+        int myNumber; //declare the variable - which is local variable
+
+        //System.out.println("MYNUMBER =" + myNumber);
+
+        myNumber = 10; //initializing a variable
+
+
+        System.out.println("MY NUMBER =" + myNumber);
+//declare variable in single line
+        double myDouble = 7.50;
+
+        System.out.println("MY Double =" + myDouble);
+
+//Declare variable in multiple lines
+
+        float myFloat, myFloat2, myFloat3;
+
+        float myFloat4; float myFloat5; float myFloat6;
+
+    }
+}
